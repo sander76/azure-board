67540b6ddd077565e2471051395ad754900a3b15
Sander Teunissen <s.teunissen@gmail.com>
2025-05-26 20:01:51 +0200

    changes

--- noxfile.py
+++ noxfile.py
@@ -1,32 +1,37 @@
-"""Nox file."""
+"""
+Nox file.
+"""
 
-import nox
+from nox import Session, options
+from nox_uv import session
 
-nox.options.default_venv_backend = "uv"
+options.default_venv_backend = "uv|virtualenv"
 
 
-@nox.session
-def test(session: nox.Session) -> None:
+@session(uv_groups=["dev"])
+def test(session: Session) -> None:
     """Testing."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--frozen",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
+    # session.install(".")
+    # session.run_install(
+    #     "uv",
+    #     "sync",
+    #     "--frozen",
+    #     env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    # )
 
     session.run("pytest")
 
 
-@nox.session(tags=["quality"])
-def quality(session: nox.Session) -> None:
+@session(tags=["quality"], uv_groups=["dev"])
+def quality(session: Session) -> None:
     """Quality checks."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--frozen",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
+    # session.install(".")
+    # session.run_install(
+    #     "uv",
+    #     "sync",
+    #     "--frozen",
+    #     env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    # )
 
     session.run("ruff", "check", "src", "tests", "noxfile.py", "--fix", "--exit-non-zero-on-fix")
     session.run("pyproject-fmt", "pyproject.toml")
